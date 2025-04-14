be30ff63c840cbbe2274f889ceb8ec11b944494d
Akshay Kalapgar <64259085+Akkikens@users.noreply.github.com>
2025-04-14 15:23:55 -0400

    changes

---
+++ client/src/components/ui/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer className="py-4 text-center bg-gray-800 border-t border-gray-700">
+      <span className="text-sm text-tertiary">
+        Made by{" "}
+        <a className="font-semibold" href="https://mattpryer.com" rel="noopener noreferrer" target="_blank">
+          Matt Pryer
+        </a>
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;

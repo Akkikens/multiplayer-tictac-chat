797e2f0dd19f137bb5556232ad208053ed2b4f9f
Akshay Kalapgar <64259085+Akkikens@users.noreply.github.com>
2025-04-15 22:04:39 -0400

    Some edits

--- client/src/components/ui/Footer.tsx
+++ client/src/components/ui/Footer.tsx
@@ -1,10 +1,10 @@
 const Footer = () => {
   return (
-    <footer className="py-4 text-center bg-gray-800 border-t border-gray-700">
-      <span className="text-sm text-tertiary">
+    <footer className="border-t border-gray-700 bg-gray-800 py-4 text-center">
+      <span className="text-tertiary text-sm">
         Made by{" "}
-        <a className="font-semibold" href="https://mattpryer.com" rel="noopener noreferrer" target="_blank">
-          Matt Pryer
+        <a className="font-semibold" href="https://akshaykalapgar.com" rel="noopener noreferrer" target="_blank">
+          Akshay Kalapgar
         </a>
       </span>
     </footer>
